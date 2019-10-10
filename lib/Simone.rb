5d0d59621dd5c77ddea732b310c5b38268490d9f
vagrant <vagrant@kleer-csd-box>
2019-10-10 17:09:14 -0300

    Comparar y validad numero

--- lib/Simone.rb
+++ lib/Simone.rb
@@ -1,25 +1,38 @@
 class Secuencia
 
 	def initialize
-	 @secuenciaInicial = [3,1,4,2,1,4,3,2]
+		@secuenciaInicial = [3,1,4,2,1,4,3,2]
 		@secuenciaActual = []
- end
+		@@ronda = 0
+ 	end
 
 	def obtener iteracion
 
 		for i in 0..iteracion do
 			@secuenciaActual.push(@secuenciaInicial[i])
 		end
-   return @secuenciaActual
+		return @secuenciaActual
 	end
 
-   def secuenciaToString 
-     @secuenciaActual.join("-")
-   end
+	def secuenciaToString 
+		@secuenciaActual.join("-")
+	end
  
- def inicial
-   @secuenciaInicial
- end
+	def inicial
+		@secuenciaInicial
+	end
 
-
+	def comparar numero
+		return @secuenciaInicial[@@ronda] == numero  
+	end
+	def incrementar_ronda
+		@@ronda+=1	
+	end
+	def comparar_y_avanzar numero
+		respuesta = comparar numero
+		if respuesta 
+			incrementar_ronda
+		end
+		return respuesta
+	end
 end
--- spec/simone_spec.rb
+++ spec/simone_spec.rb
@@ -2,26 +2,49 @@ require "./lib/Simone.rb"
 
 describe "Simone" do
 
-it "Dame una secuencia de la primera ronda" do
-	secuencia = Secuencia.new
-	expect( secuencia.obtener(0)).to eq [3]
-end
+	it "Dame una secuencia de la primera ronda" do
+		secuencia = Secuencia.new
+		expect( secuencia.obtener(0)).to eq [3]
+	end
 
-it "Dame una secuencia de la primera ronda" do
-	secuencia = Secuencia.new
-	expect( secuencia.obtener(1)).to eq [3,1]
-end
+	it "Dame una secuencia de la primera ronda" do
+		secuencia = Secuencia.new
+		expect( secuencia.obtener(1)).to eq [3,1]
+	end
 
-it "Dame la secuencia inicial" do
-  secuencia = Secuencia.new
-  expect( secuencia.inicial ).to eq [3,1,4,2,1,4,3,2]
-end
+	it "Dame la secuencia inicial" do
+	  secuencia = Secuencia.new
+	  expect( secuencia.inicial ).to eq [3,1,4,2,1,4,3,2]
+	end
 
-it "Dame la secuencia de dos numeros en string" do
-  secuencia = Secuencia.new
-  secuencia.obtener(1)
-  expect( secuencia.secuenciaToString ).to eq "3-1"
-end
+	it "Dame la secuencia de dos numeros en string" do
+	  secuencia = Secuencia.new
+	  secuencia.obtener(1)
+	  expect( secuencia.secuenciaToString ).to eq "3-1"
+	end
 
+	it "Numero ingresado por el usuario es igual a el numero correcto" do
+		secuencia = Secuencia.new
+		respuesta = secuencia.comparar(3)
+		expect(respuesta).to eq true	
+	end
+	
+	it "Numero ingresado por el usuario es igual a el numero correcto" do
+		secuencia = Secuencia.new
+		respuesta = secuencia.comparar(3)
+		expect(respuesta).to eq true	
+		secuencia.incrementar_ronda
+		respuesta = secuencia.comparar(1)
+		expect(respuesta).to eq true	
+	end
+	it "Numero ingresado por el usuario es igual a el numero correcto" do
+		secuencia = Secuencia.new
+		respuesta = secuencia.comparar_y_avanzar(3)
+		expect(respuesta).to eq true	
+		respuesta = secuencia.comparar_y_avanzar(1)
+		expect(respuesta).to eq true	
+	end
+	
+	
 
 end
