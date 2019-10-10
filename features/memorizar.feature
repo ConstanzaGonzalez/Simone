4070a24a2b9f9eb773e39115ab2700d3feb9fb91
vagrant <vagrant@kleer-csd-box>
2019-10-10 14:55:23 -0300

    second test with button avanzar

--- features/memorizar.feature
+++ features/memorizar.feature
@@ -5,3 +5,7 @@ Característica: Simone
 Escenario: Muestra secuencia numerica
 	Dado que voy a la portada memorizar
 	Entonces veo "3-2"
+
+Escenario: Muestra avanzar
+	Dado que estoy en portada memorizar
+	Entonces aparece boton "avanzar"
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -6,4 +6,11 @@ Entonces(/^veo "(.*?)"$/) do |texto|
   last_response.body.should=~ /#{texto}/m
 end
 
+Dado(/^que estoy en portada memorizar$/) do
+	visit '/memorizar'
+end
+Entonces(/^aparece boton "(.*?)"$/) do |texto|
+  last_response.body.should=~ /#{texto}/m
+end
+
 
