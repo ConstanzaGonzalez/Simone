5abc1b395088852d34b1800230ea39619d27204f
vagrant <vagrant@kleer-csd-box>
2019-10-10 15:34:50 -0300

    Secuencia inicial

--- lib/Simone.rb
+++ lib/Simone.rb
@@ -1,7 +1,21 @@
 class Secuencia
 
-	def obtener
-		"1 - 2"
+	def initialize
+	 @secuenciaInicial = [3,1,4,2,1,4,3,2]
+		@secuenciaActual = []
+ end
+
+	def obtener iteracion
+
+		for i in 0..iteracion do
+			@secuenciaActual.push(@secuenciaInicial[i])
+		end
+   return @secuenciaActual
 	end
+ 
+ def inicial
+   @secuenciaInicial
+ end
+
 
 end
--- spec/simone_spec.rb
+++ spec/simone_spec.rb
@@ -2,9 +2,20 @@ require "./lib/Simone.rb"
 
 describe "Simone" do
 
-it "dame un secuencia" do
+it "Dame una secuencia de la primera ronda" do
 	secuencia = Secuencia.new
-	expect( secuencia.obtener ).to eq "1 - 2"
+	expect( secuencia.obtener(0)).to eq [3]
 end
+
+it "Dame una secuencia de la primera ronda" do
+	secuencia = Secuencia.new
+	expect( secuencia.obtener(1)).to eq [3,1]
+end
+
+it "Dame la secuencia inicial" do
+  secuencia = Secuencia.new
+  expect( secuencia.inicial ).to eq [3,1,4,2,1,4,3,2]
+end
+
 
 end
